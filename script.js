2778df93b520bb4ced1c47bc43d0de685572f44b
Tiago <o-seu-email-do-github@exemplo.com>
2025-10-30 23:10:41 +0000

    Adicionei o ficheiro script.js com variaveis

---
+++ script.js
@@ -0,0 +1,40 @@
+console.log("O ficheiro script.js foi carregado com sucesso!");
+
+const nomeReceita ="Arroz de Pato á Moda Antiga";
+
+const tempoPreparoMinutos = 90;
+const numeroIngredientes = 7;
+
+const eReceitaDeDomingo = true;
+const eDificilDeFazer = false;
+
+const listaIngredientes = [
+    "1 pato grande", 
+    "2 Chávina de arroz", 
+    "1 cebola", 
+    "2 dentes de alho", 
+    "Chouriço", 
+    "Bacon",
+    "sal"
+];
+
+const receitaCompleta = {
+    nome: nomeReceita, 
+    tempo: tempoPreparoMinutos, 
+    listaIngredientes: listaIngredientes,
+    eDeDomingo: eReceitaDeDomingo 
+};
+
+console.log("--- Ficha Técnica da Receita ---");
+
+console.log("Nome da Receita:");
+console.log(nomeReceita);
+
+console.log("É uma receita de Domingo?");
+console.log(eReceitaDeDomingo);
+
+console.log("O terceiro ingrediente da loista é:");
+console.log(listaIngredientes[2]);
+
+console.log("A receita completa (objeto):");
+console.log(receitaCompleta);
