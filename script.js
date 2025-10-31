8063a4f2c5877cc6f5d3a5e78d8368a568984132
Tiago <o-seu-email-do-github@exemplo.com>
2025-10-31 16:24:44 +0000

    Corrigi o bug de ReferenceError na Aula 8

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
 console.log("O ficheiro script.js foi carregado com sucesso!");
 
-const nomeReceita ="Arroz de Pato á Moda Antiga";
+
+
+
+const nomeReceita = "Arroz de Pato à Moda Antiga";
+
 
 const tempoPreparoMinutos = 90;
 const numeroIngredientes = 7;
 
+
 const eReceitaDeDomingo = true;
 const eDificilDeFazer = false;
 
+
 const listaIngredientes = [
-    "1 pato grande", 
-    "2 Chávina de arroz", 
-    "1 cebola", 
-    "2 dentes de alho", 
-    "Chouriço", 
+    "1 pato grande",
+    "2 Chávinas de arroz",
+    "1 cebola",
+    "2 dentes de alho",
+    "Chouriço",
     "Bacon",
-    "sal"
+    "Sal"
 ];
 
+
 const receitaCompleta = {
-    nome: nomeReceita, 
-    tempo: tempoPreparoMinutos, 
-    listaIngredientes: listaIngredientes,
-    eDeDomingo: eReceitaDeDomingo 
+    nome: nomeReceita,
+    tempo: tempoPreparoMinutos,
+    ingredientes: listaIngredientes,
+    eDeDomingo: eReceitaDeDomingo
 };
+
 
 console.log("--- Ficha Técnica da Receita ---");
 
@@ -34,7 +42,35 @@ console.log("É uma receita de Domingo?");
 console.log(eReceitaDeDomingo);
 
 console.log("O terceiro ingrediente da lista é:");
-console.log(listaIngredientes[2]);
+console.log(listaIngredientes[2]); 
 
 console.log("A receita completa (objeto):");
 console.log(receitaCompleta);
+
+
+
+function avaliarTempoDePreparo(tempo) {
+    
+    console.log("A avaliar uma receita com " + tempo + " minutos.");
+
+
+    if (tempo < 30) {
+        return "Receita Rápida";
+    } else if (tempo <= 60) {
+        return "Receita com tempo médio";
+    } else {
+        return "Receita Demorada (prepare-se!)";
+    }
+}
+
+
+const resultadoDaNossaReceita = avaliarTempoDePreparo(tempoPreparoMinutos);
+
+console.log("--- Avaliação da Receita ---");
+
+console.log(resultadoDaNossaReceita); 
+
+
+const resultadoReceitaFacil = avaliarTempoDePreparo(20);
+console.log("Resultado para 20 min:"); 
+console.log(resultadoReceitaFacil);
